dd1364e6035afddedb8bd73ae952a226a2bc50df
Genevieve Trudel <genevievecote10@gmail.com>
2023-05-03 15:41:29 -0400

    Quelque modification Installation axios

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
-const helmet = require('helmet');
+
 const webRouter = require('./routes/web');
+
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', 'resources/views');
 
-app.use(helmet());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/', webRouter);
 app.listen(port, () => {
-console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -2106,5 +2106,7 @@
   } = axios_default;
 
   // resources/js/app.js
+  console.log("oOOOooOOO");
   axios_default.get("http://localhost:3000/api/").then((response) => console.log(response)).catch((error) => console.error(error));
+  console.log("hOnk");
 })();
